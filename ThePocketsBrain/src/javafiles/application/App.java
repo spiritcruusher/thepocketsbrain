b64f8703629456e04018a5a548a8d98d10d813f3
Othmane ATAALLAH <othmaneataallah123@gmail.com>
2022-11-28 13:12:39 +0100

    Add files via upload

---
+++ ThePocketsBrain/src/javafiles/application/App.java
@@ -0,0 +1,28 @@
+package javafiles.application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/resources/fxml/application.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("THE POCKET'S BRAIN");
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ ThePocketsBrain/src/javafiles/controllers/SignUpController.java
@@ -0,0 +1,52 @@
+package javafiles.controllers;
+
+import javafiles.models.DBUtils;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SignUpController implements Initializable {
+
+    @FXML
+    private TextField tf_username;
+    @FXML
+    private TextField tf_password;
+    @FXML
+    private Button button_signup;
+    @FXML
+    private Button button_login;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+        button_signup.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                if (!tf_username.getText().trim().isEmpty() && !tf_password.getText().trim().isEmpty()) {
+                    DBUtils.signUpUser(event, tf_username.getText(), tf_password.getText());
+                } else {
+                    System.out.println("Please fill in all information");
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setContentText("Please fill in all information to sign up!");
+                    alert.show();
+                }
+            }
+        });
+
+        button_login.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "/resources/fxml/log-in.fxml", "Log In!", null);
+            }
+        });
+
+    }
+
+}
