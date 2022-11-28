46fe6862b6df2def424cb07650139ca09fe4c2c8
Othmane ATAALLAH <othmaneataallah123@gmail.com>
2022-11-28 13:48:56 +0100

    First Commit

---
+++ src/javafiles/controllers/HomeController.java
@@ -0,0 +1,48 @@
+package javafiles.controllers;
+
+import javafiles.models.DBUtils;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class HomeController implements Initializable {
+
+    @FXML
+    private Button button_login;
+    @FXML
+    private Button button_signup;
+    @FXML
+    private Button button_menu;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+
+        button_login.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "/resources/fxml/log-in.fxml", "Log In!", null);
+            }
+        });
+
+        button_signup.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "/resources/fxml/sign-up.fxml", "Sign Up!", null);
+            }
+        });
+
+        button_menu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "/resources/fxml/application-menu.fxml", "THE POCKET'S BRAIN!", null);
+            }
+        });
+
+    }
+
+}
---
+++ src/javafiles/controllers/LogInController.java
@@ -0,0 +1,43 @@
+package javafiles.controllers;
+
+import javafiles.models.DBUtils;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LogInController implements Initializable {
+
+    @FXML
+    private Button button_login;
+    @FXML
+    private Button button_signup;
+    @FXML
+    private TextField tf_username;
+    @FXML
+    private TextField tf_password;
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        button_login.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.logInUser(event, tf_username.getText(), tf_password.getText());
+            }
+        });
+
+        button_signup.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                DBUtils.changeScene(event, "/resources/fxml/sign-up.fxml", "Sign Up!", null);
+            }
+        });
+
+    }
+
+}
